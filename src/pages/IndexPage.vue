7ce942edc8fa696d8e3c286bd363d08cc7a413dd
maximusdev1313 <maximusdev1313@gmail.com>
2023-02-15 22:00:08 +0500

    import components

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+import inputForClassInfo from 'src/components/inputsForQuestions/inputForClassInfo.vue'
+import inpForQuestion from 'src/components/inputsForQuestions/inpForQuestion.vue'
+let questionTitle = ref('')
+
+
+</script>
+
 <template>
-  <q-page class="flex flex-center">
+  <q-page >
+    <div class="row justify-around q-mt-md">
+      <Suspense>
+        <inputForClassInfo/>
+      </Suspense>
+      <inp-for-question/>
+
+      
     
+    
+    
+    </div>
+   
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<style scoped>
 
-export default defineComponent({
-  name: 'IndexPage'
-})
-</script>
+
+</style>
