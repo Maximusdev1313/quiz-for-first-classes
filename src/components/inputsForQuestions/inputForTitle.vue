82e71c2e2832d46b6a681d0052b5918b2b0ad7d5
maximusdev1313 <maximusdev1313@gmail.com>
2023-02-15 21:59:05 +0500

    inp for question title

---
+++ src/components/inputsForQuestions/inputForTitle.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+let condition = ref('')
+const addInfo = ()=>{
+     fetch("http://quizforbeginner.pythonanywhere.com/quiz/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        question_title: condition.value,
+        teacher_name: teacherName.value,
+        
+      }),
+      
+
+    });
+    store.clicker = true
+    console.log(error.message);
+  }
+
+</script>
+
+
+<template>
+    <div class="">
+          <q-input  
+          v-model="condition"
+          label="Savol"
+          label-color="primary"
+            color="white"
+            bg-color="white"
+          />
+        </div>
+</template>
