9a54d4baca0db461fe1adacf320b5909dea793a4
maximusdev1313 <maximusdev1313@gmail.com>
2023-02-15 21:57:59 +0500

    create inputs for variants

---
+++ src/components/inputsForQuestions/inpForVariants.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div class="row justify-between items-center">
+    <div>
+        <q-input
+      v-model="model"
+      label-color="primary"
+      color="white"
+      bg-color="white"
+    />
+    <q-checkbox v-model="val" label="To'g'ri/Noto'g'ri" />
+    </div>
+    <div>
+        <q-input
+      v-model="model"
+      label-color="primary"
+      color="white"
+      bg-color="white"
+    />
+    <q-checkbox v-model="val" label="To'g'ri/Noto'g'ri" />
+    </div>
+    <div>
+        <q-input
+      v-model="model"
+      label-color="primary"
+      color="white"
+      bg-color="white"
+    />
+    <q-checkbox v-model="val" label="To'g'ri/Noto'g'ri" />
+    </div>
+    
+    
+  </div>
+</template>
