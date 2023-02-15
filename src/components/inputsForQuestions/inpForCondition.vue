ebfb7c7bd6b4c4a6fc238a4a20e8fc4e4d434efe
maximusdev1313 <maximusdev1313@gmail.com>
2023-02-15 21:56:53 +0500

    create inputs for condition of tests

---
+++ src/components/inputsForQuestions/inpForCondition.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref } from 'vue';
+
+let model = ref('')
+let val = ref(false)
+</script>
+
+<template>
+    <div>
+        <div class="row justify-between no-wrap q-mt-md">
+            <div>
+                <q-input  
+                    v-model="model"
+                    
+                    label-color="primary"
+                    color="white"
+                    bg-color="white"
+                />
+                <q-checkbox v-model="val" label="Berkitish" />
+                
+            </div>
+            <div>
+                <q-input  
+                    v-model="model"
+                    
+                    label-color="primary"
+                    color="white"
+                    bg-color="white"
+                />
+                <q-checkbox v-model="val" label="Berkitish" />
+                
+            </div>
+            <div>
+                <q-input  
+                    v-model="model"
+                    
+                    label-color="primary"
+                    color="white"
+                    bg-color="white"
+                />
+                <q-checkbox v-model="val" label="Berkitish" />
+                
+            </div>
+            <div>
+                <q-input  
+                    v-model="model"
+                    
+                    label-color="primary"
+                    color="white"
+                    bg-color="white"
+                />
+                <q-checkbox v-model="val" label="Berkitish" />
+                
+            </div>
+            <div>
+                <q-input  
+                    v-model="model"
+                    
+                    label-color="primary"
+                    color="white"
+                    bg-color="white"
+                />
+                <q-checkbox v-model="val" label="Berkitish" />
+                
+            </div>
+          
+        </div>
+
+    </div>
+</template>
