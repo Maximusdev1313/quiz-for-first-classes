e83c495082a2eb01d64733cb07906cd58b245cff
maximusdev1313 <maximusdev1313@gmail.com>
2023-02-17 01:19:37 +0500

    not ready

--- src/components/inputsForQuestions/inpForVariants.vue
+++ src/components/inputsForQuestions/inpForVariants.vue
@@ -1,5 +1,10 @@
 <script setup>
+import { ref } from 'vue';
+import {useApiStore} from 'src/stores'
+const store = useApiStore()
 
+let variant  = ref('')
+let trueOrFalseAnswer = ref(false)
 
 </script>
 
@@ -7,30 +12,12 @@
   <div class="row justify-between items-center">
     <div>
         <q-input
-      v-model="model"
+      v-model="variant"
       label-color="primary"
       color="white"
       bg-color="white"
     />
-    <q-checkbox v-model="val" label="To'g'ri/Noto'g'ri" />
-    </div>
-    <div>
-        <q-input
-      v-model="model"
-      label-color="primary"
-      color="white"
-      bg-color="white"
-    />
-    <q-checkbox v-model="val" label="To'g'ri/Noto'g'ri" />
-    </div>
-    <div>
-        <q-input
-      v-model="model"
-      label-color="primary"
-      color="white"
-      bg-color="white"
-    />
-    <q-checkbox v-model="val" label="To'g'ri/Noto'g'ri" />
+    <q-checkbox v-model="trueOrFalseAnswer" label="To'g'ri/Noto'g'ri" />
     </div>
     
     
