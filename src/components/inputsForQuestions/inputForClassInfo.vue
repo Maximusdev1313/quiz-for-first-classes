46fe984dee5ad57b07bbf94055d4996ca4d2d511
maximusdev1313 <maximusdev1313@gmail.com>
2023-02-17 01:20:19 +0500

    making logical side

--- src/components/inputsForQuestions/inputForClassInfo.vue
+++ src/components/inputsForQuestions/inputForClassInfo.vue
@@ -1,25 +1,35 @@
 <script setup>
 import {ref} from 'vue'
+import { useRoute } from 'vue-router';
 import {useApiStore} from 'src/stores'
-
+const route = useRoute()
 const store = useApiStore()
 let className = ref('')
 let teacherName = ref('')
+
+let date = new Date().getTime();
+const random = Math.floor(Math.random() * 10000);
+let special_id = random.toString() + date + random.toString();
+
 let clicker = ref(false)
 const addInfo = ()=>{
+      store.SetItemsTostorage('idForInfo', special_id)
+      let infoId = store.GetItemFromStorage('idForInfo')
      fetch("http://quizforbeginner.pythonanywhere.com/class/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
+        id: infoId,
         class_name: className.value,
         teacher_name: teacherName.value,
         
       }),
-      
 
     });
-    store.clicker = true
-    store.SetItemsTostorage('class_name', className.value)
+    store.GetItemFromStorage('idForInfo')
+    console.log(store.GetItemFromStorage('idForInfo'));
+    // store.clicker = true
+    console.log(store.special_id_for_info);
   }
 
 </script>
