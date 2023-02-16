55b760dc3a526387242b8878ac4b59549987eaa3
maximusdev1313 <maximusdev1313@gmail.com>
2023-02-17 01:22:04 +0500

    create special id

--- src/components/inputsForQuestions/inputForTitle.vue
+++ src/components/inputsForQuestions/inputForTitle.vue
@@ -1,33 +1,47 @@
 <script setup>
-import { ref } from 'vue';
-let condition = ref('')
-const addInfo = ()=>{
-     fetch("http://quizforbeginner.pythonanywhere.com/quiz/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        question_title: condition.value,
-        teacher_name: teacherName.value,
-        
-      }),
-      
+import { ref } from "vue";
+import { useApiStore } from "src/stores";
+const store = useApiStore();
+let quiz = ref("");
 
-    });
-    store.clicker = true
-    console.log(error.message);
-  }
+let date = new Date().getTime();
+const random = Math.floor(Math.random() * 10000);
+let special_id = random.toString() + date + random.toString();
 
+let idTitle = ref("");
+let parentId = ref("");
+const addInfo = (setFunction) => {
+  setFunction();
+  fetch("http://quizforbeginner.pythonanywhere.com/quiz/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      id: idTitle.value,
+      question_title: quiz.value,
+      quizzes: parentId.value,
+    }),
+  });
+  console.log("nimadur");
+};
+let setGetItems = () => {
+  store.SetItemsTostorage("idForTitle", special_id);
+  idTitle.value = store.GetItemFromStorage("idForTitle");
+  parentId.value = store.GetItemFromStorage("idForInfo");
+
+  console.log(idTitle);
+  console.log(parentId);
+};
 </script>
 
-
 <template>
-    <div class="">
-          <q-input  
-          v-model="condition"
-          label="Savol"
-          label-color="primary"
-            color="white"
-            bg-color="white"
-          />
-        </div>
+  <div class="">
+    <q-input
+      v-model="quiz"
+      label="Savol"
+      @keydown.tab="addInfo(setGetItems)"
+      label-color="primary"
+      color="white"
+      bg-color="white"
+    />
+  </div>
 </template>
